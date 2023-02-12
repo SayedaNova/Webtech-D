bd36df2ece39b2aa1bc544813d63696f766b9975
SayedaNova <120387417+SayedaNova@users.noreply.github.com>
2023-02-12 21:05:06 +0600

    lab2

---
+++ lab2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title></title>
+</head>
+<body>
+	
+	<fieldset>
+		<legend><h3><B>REGISTRATION</B></h3> </legend>
+	
+	<label for="name"> <B> Name:</B></label>
+	<input type="text" name="name"> <br>
+    <hr>
+	
+	
+	<label for="Email"><B>Email:</B></label>
+    <input type="Email" name="Email"><br>
+    <hr>
+    <label for="u_name"><B>User Name:</B></label>
+    <input type="text" name="U_name"><br>
+    <hr>
+    <label for="password"><B>Password:</B></label>
+    <input type="password" name="password"><br>
+    <hr>
+    <label for="cpass"><B>Confirm password :</B></label>
+    <input type="text" name="cpass"><br>
+    <hr>
+    <fieldset>
+    
+      <legend>Gender</legend>
+      <input type="radio" name="male">
+      <label for="male"> <B>Male</B></label>
+      <input type="radio" name="fmale">
+      <label for="fmale"> <B>Female</B></label>
+      <input type="radio" name="other">
+      <label for="other"><B> Other</B></label><br>
+    
+    </fieldset><br>
+    <fieldset>
+    	<legend>Date of Birth</legend>
+        <input type="text" name="date">/
+        <input type="text" name="month">/
+        <input type="text" name="year"> <label>(dd/mm/yyy)</label>
+    </fieldset><br>
+    <button> Submit</button>
+    <button> Reset</button>
+    </fieldset>
+	
+</body>
+</html>
