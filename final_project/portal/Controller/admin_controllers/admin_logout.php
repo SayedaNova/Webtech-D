8c358d9480ebd0e6c11d21b45b8da484163bbeb2
SayedaNova <120387417+SayedaNova@users.noreply.github.com>
2023-05-10 15:19:30 +0600

    Delete final_project/portal directory

--- final_project/portal/Controller/admin_controllers/admin_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	require "db_con.php";
-	require_once "functions.php";
-	$user = new admin_class();
-	$user->admin_logout(); //calling admin_logout function
-	header('Location: admin_login.php');
-	exit();
-?>
