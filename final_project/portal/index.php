de062bd7643d4e97d4313e4eee1a2e9aaf53875b
SayedaNova <120387417+SayedaNova@users.noreply.github.com>
2023-05-10 14:15:30 +0600

    Add files via upload

---
+++ final_project/portal/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HOME</title>
+</head>
+<body>
+
+	<h1>Welcome to our Student Portal.</h1>
+	<h3>Login as:</h4>
+	<a href="View/admin_views/admin_login_view.php">Admin</a>
+	<a href="View/student_views/student_login_view.php">Student</a>
+	<a href="View/teacher_views/teacher_login_view.php">Teacher</a>
+
+	<h3>Register as:</h3>
+	<a href="View/student_views/signup_view.php">New Student</a><br>
+	<a href="View/teacher_views/signup_view.php">New Teacher</a>
+
+</body>
+</html>
